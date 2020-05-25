62e2ef8775fe19a3a192aec0c54e5061d4e51c77
Ra0l <45242019+Ra0l@users.noreply.github.com>
2020-05-24 19:51:53 -0500

    Agregar archivos de proyecto.

---
+++ MVCEjemplo/Controllers/MovieController.cs
@@ -0,0 +1,119 @@
+﻿using MVCEjemplo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCEjemplo.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Index()
+        {
+            List<MovieModel> models = new List<MovieModel>();
+            //Ir a la base de datos a llenar a la lista de models
+
+            if (Session["Movies"] == null)
+            {
+                models.Add(new MovieModel { MovieID = 1, Title = "Cinemark", Price = 35 });
+                models.Add(new MovieModel { MovieID = 2, Title = "CinePlanet", Price = 40 });
+                Session["Movies"] = models;
+            }
+            else
+            {
+                models = (List<MovieModel>)Session["Movies"];
+            }
+
+            return View(models);
+        }
+
+        // GET: Movie/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Movie/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Movie/Create
+        [HttpPost]
+        public ActionResult Create(MovieModel model)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                ((List<MovieModel>)Session["Movies"]).Add(model);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Movie/Edit/5
+        public ActionResult Edit(int id)
+        {
+            MovieModel model = ((List<MovieModel>)Session["Movies"]).
+                                FirstOrDefault(x => x.MovieID == id);
+            return View(model);
+        }
+
+        // POST: Movie/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, MovieModel model)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                MovieModel modelAnt = ((List<MovieModel>)Session["Movies"]).
+                                FirstOrDefault(x => x.MovieID == id);
+
+                //Asignar valores nuevos 
+                modelAnt.MovieID = model.MovieID;
+                modelAnt.Title = model.Title;
+                model.Price = model.Price;
+
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Movie/Delete/5
+        public ActionResult Delete(int id)
+        {
+            MovieModel model = ((List<MovieModel>)Session["Movies"]).
+                                FirstOrDefault(x => x.MovieID == id);
+            return View(model);
+        }
+
+        // POST: Movie/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, MovieModel model)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                MovieModel modelAnt = ((List<MovieModel>)Session["Movies"]).
+                                FirstOrDefault(x => x.MovieID == id);
+
+                ((List<MovieModel>)Session["Movies"]).Remove(modelAnt);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MVCEjemplo/Controllers/ProductController.cs
@@ -0,0 +1,122 @@
+﻿using MVCEjemplo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCEjemplo.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            List<ProductModel> models = new List<ProductModel>();
+            if (Session["Products"]==null)
+            {             
+                models.Add(new ProductModel { ProductID = 1, Name = "Laptop" });
+                models.Add(new ProductModel { ProductID = 2, Name = "Mouse" });
+                Session["Products"] = models;
+            }
+            else
+            {
+                models =(List<ProductModel>) Session["Products"];
+            }
+            return View(models);
+        } 
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        public ActionResult Create(ProductModel model)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                ((List<ProductModel>)Session["Products"]).Add(model);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            ProductModel model = ((List<ProductModel>)Session["Products"]).
+                                  FirstOrDefault(x => x.ProductID == id);
+            return View(model);
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, ProductModel model)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                ProductModel modelAnt = ((List<ProductModel>)Session["Products"]).
+                                  FirstOrDefault(x => x.ProductID == id);
+                
+                //Asignar valores nuevos
+                modelAnt.ProductID = model.ProductID;
+                modelAnt.IsEnable = model.IsEnable;
+                modelAnt.Price = model.Price;
+                modelAnt.Stock = model.Stock;
+                modelAnt.Type = model.Type;
+                modelAnt.Name = model.Name;
+
+
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete(int id)
+        {
+            ProductModel model = ((List<ProductModel>)Session["Products"]).
+                                FirstOrDefault(x => x.ProductID == id);
+            return View(model);
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, ProductModel model)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                ProductModel modelAnt = ((List<ProductModel>)Session["Products"]).
+                                 FirstOrDefault(x => x.ProductID == id);
+
+                ((List<ProductModel>)Session["Products"]).Remove(modelAnt);
+
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
